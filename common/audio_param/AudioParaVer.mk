680c8b1290a92cd94883af7cc9739644aa7e4013
zhouzuoyu <zhouzuoyu@huaqin.com>
2017-03-13 10:24:51 +0800

    MANNING-422: audio: optimize audio parameter 主要调试 宽频客观通话指标 Signed-off-by: zhouzuoyu <zhouzuoyu@huaqin.com>
    Change-Id: I9f82098f2513944b38eda3c4d98c39a54ebcd68b

--- common/audio_param/AudioParaVer.mk
+++ common/audio_param/AudioParaVer.mk
@@ -3,5 +3,6 @@
 #
 # history:
 #  2017-03-09 本次主要优化通话主客观和产线/ALT所报问题
-AUDIO_PARA_VER := MANNING-DVT2-2017-3-9
+#  2017-03-13 主要调试 宽频客观通话指标
+AUDIO_PARA_VER := MANNING-DVT2-2017-3-13
 PRODUCT_PROPERTY_OVERRIDES += persist.audio.lenovo.version=$(AUDIO_PARA_VER)
