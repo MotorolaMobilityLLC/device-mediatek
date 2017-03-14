abaed0d9ee7da4ee113de4fc43e8f259a8fce858
miaotao1 <miaotao1@lenovo.com>
2017-03-14 12:39:09 +0800

    OTA:adding scatter and type file in validation package
    Change-Id: Id693db5f667f3e3ba2d6ebc1169e8aebdf4a7140
    Reviewed-on: https://gerrit.mot.com/961500
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: tao miao <miaotao1@mt.com>
    Reviewed-by: zhaolin teng <tengzl1@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- build/releasetools/mt_ota_from_target_files.py
+++ build/releasetools/mt_ota_from_target_files.py
@@ -228,3 +228,18 @@ def IncrementalOTA_InstallEnd(self):
 
   # add extra images to upgrade
   AddOTAImage_Items(target_zip, output_zip, tgt_info_dict, script, source_zip=source_zip)
+
+#Start, lenovo-sw, miaotao1, add scatter.txt and type.txt for verify package
+def VerifyOTA_Assertions(self):
+  input_zip = self.input_zip
+  input_version= self.input_version
+  output_zip = self.output_zip
+  script = self.script
+  input_tmp = self.input_tmp
+  metadata = self.metadata
+  info_dict = self.info_dict
+
+  # add OTA information
+  AddOTA_Items(input_zip, output_zip, 1)
+
+#End, lenovo-sw, miaotao1, add scatter.txt and type.txt for verify package
