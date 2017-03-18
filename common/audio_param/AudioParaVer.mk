bad737489986bad56a3e95b1012602e9ea3f7d0b
yiyinglin <yiyinglin@huaqin.com>
2017-03-18 18:01:41 +0800

    MANNING-468: audio: optimize audio parameter
    主要调试 改善杂音
    
    Change-Id: Ie367d88ce2ff0e93bf6c0199a1fe5c51a45e3ce6
    Signed-off-by: yiyinglin <yiyinglin@huaqin.com>

--- common/audio_param/AudioParaVer.mk
+++ common/audio_param/AudioParaVer.mk
@@ -4,5 +4,6 @@
 # history:
 #  2017-03-09 本次主要优化通话主客观和产线/ALT所报问题
 #  2017-03-13 主要调试 宽频客观通话指标
-AUDIO_PARA_VER := MANNING-DVT2-2017-3-13
+#  2017-03-18 主要调试 改善杂音
+AUDIO_PARA_VER := MANNING-DVT2-2017-3-18
 PRODUCT_PROPERTY_OVERRIDES += persist.audio.lenovo.version=$(AUDIO_PARA_VER)
