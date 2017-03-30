f2a0d53136cb373eb41ee502fa85bdcdb68b26e4
yiyinglin <yiyinglin@huaqin.com>
2017-03-30 18:13:17 +0800

    MANNING-533:audio: optimize audio parameter
    本次参数主要优化免提上下行通话和降噪效果
    
    Change-Id: I140617baee90a8adc6b2e8bed4f5472d943b48df
    Signed-off-by: yiyinglin <yiyinglin@huaqin.com>

--- common/audio_param/AudioParaVer.mk
+++ common/audio_param/AudioParaVer.mk
@@ -5,5 +5,6 @@
 #  2017-03-09 本次主要优化通话主客观和产线/ALT所报问题
 #  2017-03-13 主要调试 宽频客观通话指标
 #  2017-03-18 主要调试 改善杂音
-AUDIO_PARA_VER := MANNING-DVT2-2017-3-18
+#  2017-03-30 主要优化免提上下行通话和降噪效
+AUDIO_PARA_VER := MANNING-DVT2-2017-3-30
 PRODUCT_PROPERTY_OVERRIDES += persist.audio.lenovo.version=$(AUDIO_PARA_VER)
