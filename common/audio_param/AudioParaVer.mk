ca61e53fb8d0678c6b078a9f2636197b9f45b6e8
yiyinglin <yiyinglin@huaqin.com>
2017-04-05 19:52:18 +0800

    MANNING-580:audio: optimize audio parameter
    解决联想所报音阶调整为7阶后音量变小问题
    
    Change-Id: I5b21fa93795fc94b9ff437d3f9bd7e61b09a9b37
    Signed-off-by: yiyinglin <yiyinglin@huaqin.com>

--- common/audio_param/AudioParaVer.mk
+++ common/audio_param/AudioParaVer.mk
@@ -6,5 +6,6 @@
 #  2017-03-13 主要调试 宽频客观通话指标
 #  2017-03-18 主要调试 改善杂音
 #  2017-03-30 主要优化免提上下行通话和降噪效
+#  2017-04-05 解决联想所报音阶调整为7阶后音量变小问题
 AUDIO_PARA_VER := MANNING-DVT2-2017-3-30
 PRODUCT_PROPERTY_OVERRIDES += persist.audio.lenovo.version=$(AUDIO_PARA_VER)
