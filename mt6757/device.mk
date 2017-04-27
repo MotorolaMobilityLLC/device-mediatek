173df6d20b953a0ed79b01576592438488620c9a
tianqiangqiang <tianqiangqiang@longcheer.net>
2017-04-27 15:53:28 +0800

    [Purp][L3500][tee-32]open tee feture

--- mt6757/device.mk
+++ mt6757/device.mk
@@ -1425,7 +1425,7 @@ endif
 ifeq ($(strip $(MICROTRUST_TEE_SUPPORT)), yes)
 PRODUCT_COPY_FILES += 	device/mediatek/mt6757/init.microtrust.rc:root/init.microtrust.rc
 PRODUCT_COPY_FILES += 	vendor/mediatek/proprietary/trustzone/microtrust/source/platform/mt6757/teei/soter.raw:$(TARGET_COPY_OUT_VENDOR)/thh/soter.raw:mtk
-PRODUCT_COPY_FILES += 	vendor/mediatek/proprietary/trustzone/microtrust/source/platform/mt6757/teei/fp_server:$(TARGET_COPY_OUT_VENDOR)/thh/fp_server:mtk
+#PRODUCT_COPY_FILES += 	vendor/mediatek/proprietary/trustzone/microtrust/source/platform/mt6757/teei/fp_server:$(TARGET_COPY_OUT_VENDOR)/thh/fp_server:mtk
 PRODUCT_COPY_FILES += 	vendor/mediatek/proprietary/trustzone/microtrust/source/platform/mt6757/teei/init_thh:$(TARGET_COPY_OUT_VENDOR)/bin/init_thh:mtk
 PRODUCT_COPY_FILES += 	vendor/mediatek/proprietary/trustzone/microtrust/source/platform/mt6757/teei/uTAgent:$(TARGET_COPY_OUT_VENDOR)/thh/uTAgent
 PRODUCT_COPY_FILES += 	vendor/mediatek/proprietary/trustzone/microtrust/source/platform/mt6757/teei/thh_mt6757_lenovo_indriya.cfg:cache/thh/tee_00/thh_mt6757_lenovo_indriya.cfg
@@ -1503,12 +1503,12 @@ ifeq ($(strip $(MTK_AOD_SUPPORT)), yes)
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.mtk_aod_support=1
 endif
 
-PRODUCT_COPY_FILES += \
-    $(GOODIX_PATH)/libgf_hal.so:system/lib64/libgf_hal.so \
-    $(GOODIX_PATH)/libgf_algo.so:system/lib64/libgf_algo.so \
-    $(GOODIX_PATH)/fingerprint.default.so:system/lib64/hw/fingerprint.default.so \
-    $(GOODIX_PATH)/libgf_ca.so:system/lib64/libgf_ca.so \
-    $(GOODIX_PATH)/goodixfingerprintd:system/bin/goodixfingerprintd \
-    $(GOODIX_PATH)/libgoodixfingerprintd_binder.so:system/lib64/libgoodixfingerprintd_binder.so
+#PRODUCT_COPY_FILES += \
+#    $(GOODIX_PATH)/libgf_hal.so:system/lib/libgf_hal.so \
+#    $(GOODIX_PATH)/libgf_algo.so:system/lib/libgf_algo.so \
+#    $(GOODIX_PATH)/fingerprint.default.so:system/lib/hw/fingerprint.default.so \
+#    $(GOODIX_PATH)/libgf_ca.so:system/lib/libgf_ca.so \
+#    $(GOODIX_PATH)/goodixfingerprintd:system/bin/goodixfingerprintd \
+#    $(GOODIX_PATH)/libgoodixfingerprintd_binder.so:system/lib/libgoodixfingerprintd_binder.so
   
 
