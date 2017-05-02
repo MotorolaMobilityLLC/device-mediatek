f168a0d750fee8e7a1064e20b4a1fe7610ab79cd
Dingwei Luo <luodw1@motorola.com>
2017-05-02 03:32:07 -0500

    Revert "INDRIYA-19 PGPT: Fix partition GUID tables number calculation error"
    This reverts commit 658de4d61560fab5c6fe6c605017823a9d7ff3df.
    
    Change-Id: I4c410ee4b448b33f3c1d2e1d29b25e2954398032
    Reviewed-on: https://gerrit.mot.com/987504
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- build/build/tools/ptgen/MT6757/ptgen.pl
+++ build/build/tools/ptgen/MT6757/ptgen.pl
@@ -1556,8 +1556,8 @@ sub GenPGPTFile
    print FH pack("Q",$FirstUsableLBA);
    seek(FH,0x48,0);
    print FH pack("C8",0x02,0x00,0x00,0x00,0x00,0x00,0x00,0x00);
-   #seek(FH,0x50,0);
-   #print FH pack("L",$MAX_GPT_PARTITION_NUM);
+   seek(FH,0x50,0);
+   print FH pack("L",$MAX_GPT_PARTITION_NUM);
    seek(FH,0x54,0);
    print FH pack("L",$GPT_ENTRY_SIZE);
 
@@ -1616,10 +1616,7 @@ sub GenPGPTFile
          $Wide_Partition_Name .= "\0";
       }
       print FH pack("a72",$Wide_Partition_Name);
-      $MAX_GPT_PARTITION_NUM++;
    }
-   seek(FH,0x50,0); #notice not 128 for D1
-   print FH pack("L",$MAX_GPT_PARTITION_NUM);
 
    close(FH);
 }
