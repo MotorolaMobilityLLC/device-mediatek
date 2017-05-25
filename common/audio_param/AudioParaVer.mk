3bb337dd73b6919788c17ad454a8e44ee6a3383f
chenxym <chenxym@motorola.com>
2017-05-25 18:28:24 +0800

    INDRIYAPRO-1684:update audio handfree parameters
    mtk improve handfree parameters
    
    Change-Id: If475434913aeecd38db3deb32bfd46978bd0e3b8
    Reviewed-on: https://gerrit.mot.com/1002117
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- common/audio_param/AudioParaVer.mk
+++ common/audio_param/AudioParaVer.mk
@@ -12,5 +12,6 @@
 #				1、录音录像底噪偏大，人声失真，调试Audio Record,增益和NR参数；
 #				2、免提通话效果稍差，调试handfree -NB 模式NR参数；
 #				3、耳机通话底噪大稍大，调试headset 模式 NR参数。
-AUDIO_PARA_VER := MANNING-DVT2-2017-5-15
+# 2017-05-25 mtk优化免提参数，修改SPE参数以及Analog Gain
+AUDIO_PARA_VER := MANNING-DVT2-2017-5-25
 PRODUCT_PROPERTY_OVERRIDES += persist.audio.lenovo.version=$(AUDIO_PARA_VER)
